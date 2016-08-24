6250afa4f031af1562de1041a0c62519b826aeda
Seleznev Alex <Seleznevam84@ya.ru>
2016-08-24 12:01:34 +0300

    Залогировал жизненный цикл активити

--- app/src/main/java/com/example/alex/skillintensive/ui/activities/MainActivity.java
+++ app/src/main/java/com/example/alex/skillintensive/ui/activities/MainActivity.java
@@ -2,14 +2,54 @@ package com.example.alex.skillintensive.ui.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.example.alex.skillintensive.R;
+import com.example.alex.skillintensive.util.ConstantManager;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = ConstantManager.LOG_PREFIX +  " mainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d(TAG, "onCreate");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause");
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        Log.d(TAG, "onSaveInstanceState");
     }
 }
---
+++ app/src/main/java/com/example/alex/skillintensive/util/ConstantManager.java
@@ -0,0 +1,8 @@
+package com.example.alex.skillintensive.util;
+
+/**
+ * Created by POLOUSOV on 24.08.2016.
+ */
+public interface ConstantManager {
+    String LOG_PREFIX = "Dev";
+}
