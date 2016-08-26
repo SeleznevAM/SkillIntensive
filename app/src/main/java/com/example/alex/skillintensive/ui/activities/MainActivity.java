2bd8b8b2366c49f56351d42b84d8643053691e46
Seleznevam <o7h6hzyn>
2016-08-26 20:10:45 +0300

    Добавили ТулБар и ФАБ

--- app/src/main/java/com/example/alex/skillintensive/ui/activities/MainActivity.java
+++ app/src/main/java/com/example/alex/skillintensive/ui/activities/MainActivity.java
@@ -2,9 +2,17 @@ package com.example.alex.skillintensive.ui.activities;
 
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 
@@ -14,6 +22,9 @@ import com.example.alex.skillintensive.util.ConstantManager;
 public class MainActivity extends BaseActivity implements View.OnClickListener {
     private static final String TAG = ConstantManager.LOG_PREFIX +  " mainActivity";
     private ImageView mPhoneImage;
+    private Toolbar mToolbar;
+    private DrawerLayout mNAvigationDrawer;
+    private CoordinatorLayout mCoordinatorLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,7 +32,13 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
         setContentView(R.layout.activity_main);
         Log.d(TAG, "onCreate");
         mPhoneImage = (ImageView) findViewById(R.id.phone_img);
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        mNAvigationDrawer = (DrawerLayout) findViewById(R.id.navigation_drawer);
+        mCoordinatorLayout = (CoordinatorLayout) findViewById(R.id.coordinator_container);
         mPhoneImage.setOnClickListener(this);
+
+        setupToolbar();
+        setupDrawer();
     }
 
     @Override
@@ -67,5 +84,39 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
                 showProgress();
                 break;
         }
+    }
+
+    private void showSnackbar(String message){
+        Snackbar.make(mCoordinatorLayout, message,Snackbar.LENGTH_LONG).show();
+    }
+
+    private void setupToolbar(){
+        setSupportActionBar(mToolbar);
+        ActionBar actionBar  = getSupportActionBar();
+        if(actionBar!=null){
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_menu_black_24dp); //Устновить кнопку
+            actionBar.setDisplayHomeAsUpEnabled(true);  //ПРоказывать кнопку
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) { //Отрабатывает клик по элементам меню Toolbar
+        if(item.getItemId()== android.R.id.home){ //Если элемент меню - это кнопка Home то
+            mNAvigationDrawer.openDrawer(GravityCompat.START); //показать боковое меню слева
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void setupDrawer(){
+        final NavigationView navigationView = (NavigationView) findViewById(R.id.navigation_view);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem item) {
+                showSnackbar(item.getTitle().toString());
+                item.setChecked(true);
+                mNAvigationDrawer.closeDrawer(GravityCompat.START);
+                return false;
+            }
+        });
     }
 }
