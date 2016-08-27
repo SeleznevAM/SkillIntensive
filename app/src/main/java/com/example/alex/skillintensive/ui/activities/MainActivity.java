662616e15a2c42e4bb9dbdaf17f7156d89ddf234
Seleznevam <o7h6hzyn>
2016-08-27 19:44:47 +0300

    Добавили SharedPreference

--- app/src/main/java/com/example/alex/skillintensive/ui/activities/MainActivity.java
+++ app/src/main/java/com/example/alex/skillintensive/ui/activities/MainActivity.java
@@ -1,23 +1,30 @@
 package com.example.alex.skillintensive.ui.activities;
 
-import android.content.SharedPreferences;
-import android.preference.PreferenceManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.provider.ContactsContract;
 import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
 
 import com.example.alex.skillintensive.R;
+import com.example.alex.skillintensive.data.managers.DataManager;
 import com.example.alex.skillintensive.util.ConstantManager;
+import com.example.alex.skillintensive.util.RoundedAvatarDrawable;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends BaseActivity implements View.OnClickListener {
     private static final String TAG = ConstantManager.LOG_PREFIX +  " mainActivity";
@@ -25,6 +32,12 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
     private Toolbar mToolbar;
     private DrawerLayout mNAvigationDrawer;
     private CoordinatorLayout mCoordinatorLayout;
+    private FloatingActionButton mFab;
+    private EditText mUserPhone, mUserMail, mUserVk, mUserGit, mUSerBio;
+    private int mCurrentMode =0; //Переключатель  для опеределения режима редактирования
+    private DataManager mDataManager;
+    private ImageView mUserAvatar;
+    private List<EditText> mUserInfoViews;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,10 +48,43 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
         mToolbar = (Toolbar) findViewById(R.id.toolbar);
         mNAvigationDrawer = (DrawerLayout) findViewById(R.id.navigation_drawer);
         mCoordinatorLayout = (CoordinatorLayout) findViewById(R.id.coordinator_container);
-        mPhoneImage.setOnClickListener(this);
+        mFab = (FloatingActionButton) findViewById(R.id.fab);
+        mUserPhone = (EditText) findViewById(R.id.user_phone_et);
+        mUserMail = (EditText) findViewById(R.id.user_mail_et);
+        mUserVk = (EditText) findViewById(R.id.user_vk_et);
+        mUserGit = (EditText) findViewById(R.id.user_git_et);
+        mUSerBio = (EditText) findViewById(R.id.user_bio_et);
+        mUserAvatar = (ImageView) findViewById(R.id.user_avatar_img);
+        mDataManager = DataManager.getInstance();
 
+        /**
+         * Заполняем массив Вьюх пользовательских данных! Приоретено использовать библиотеку Butter_knife
+         */
+        mUserInfoViews = new ArrayList<>();
+        mUserInfoViews.add(mUserPhone);
+        mUserInfoViews.add(mUserMail);
+        mUserInfoViews.add(mUserVk);
+        mUserInfoViews.add(mUserGit);
+        mUserInfoViews.add(mUSerBio);
+
+
+        mFab.setOnClickListener(this);
+        mPhoneImage.setOnClickListener(this);
         setupToolbar();
         setupDrawer();
+        loadUserInfoValue();
+
+
+
+        List<String> test = mDataManager.getPreferenceManager().loadUserProfileData();
+
+
+        if(savedInstanceState==null){
+
+        }else{
+            mCurrentMode = savedInstanceState.getInt(ConstantManager.EDIT_MODE_KEY, 0);
+            changeEditMode(mCurrentMode);
+        }
     }
 
     @Override
@@ -63,6 +109,7 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
     protected void onStop() {
         super.onStop();
         Log.d(TAG, "onStop");
+        saveUserInfoValue();
     }
 
     @Override
@@ -75,6 +122,7 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
         Log.d(TAG, "onSaveInstanceState");
+        outState.putInt(ConstantManager.EDIT_MODE_KEY,mCurrentMode);
     }
 
     @Override
@@ -82,6 +130,17 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
         switch(v.getId()){
             case R.id.phone_img:
                 showProgress();
+                break;
+            case R.id.fab:
+                if(mCurrentMode == 0){
+                    changeEditMode(1);
+                    showSnackbar("Режим редактирования");
+                    mCurrentMode = 1;
+                }else{
+                    changeEditMode(0);
+                    mCurrentMode =0;
+                }
+
                 break;
         }
     }
@@ -118,5 +177,47 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
                 return false;
             }
         });
+    }
+
+    /**
+     * Переключает режим редактирования
+     * @param mode ксли 1 - режим редактирования если 0 режим просмотра
+     */
+    private void changeEditMode(int mode) {
+        if (mode == 1) {
+            mFab.setImageResource(R.drawable.ic_done_black_24dp);
+            for (EditText userValue : mUserInfoViews) {
+                userValue.setEnabled(true);
+                userValue.setFocusable(true);
+                userValue.setFocusableInTouchMode(true);
+            }
+        } else {
+            mFab.setImageResource(R.drawable.ic_edit_black_24dp);
+            for (EditText userValue : mUserInfoViews) {
+                userValue.setFocusable(false);
+                userValue.setEnabled(false);
+
+            }
+        }
+    }
+
+    /**
+     * Загрузка пользовательских данных
+     */
+    private void loadUserInfoValue(){
+        List<String> userData = mDataManager.getPreferenceManager().loadUserProfileData();
+        for (int i = 0; i <userData.size() ; i++) {
+            mUserInfoViews.get(i).setText(userData.get(i));
+        }
+    }
+    /**
+     * Сохранение пользовательских данных
+     */
+    private void saveUserInfoValue(){
+        List<String> userData  = new ArrayList<>();
+        for (EditText userFieldView  : mUserInfoViews) {
+            userData.add(userFieldView.getText().toString());
+        }
+        mDataManager.getPreferenceManager().saveUserProfileData(userData);
     }
 }
---
+++ app/src/main/java/com/example/alex/skillintensive/util/SkillintensiveApplication.java
@@ -0,0 +1,19 @@
+package com.example.alex.skillintensive.util;
+
+import android.app.Application;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
+public class SkillintensiveApplication extends Application {
+    private static SharedPreferences sSharedPreferences;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        sSharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);   //Получаем SharedPreferences
+    }
+
+    public static SharedPreferences getSharedPreferences() {
+        return sSharedPreferences;
+    }
+}
